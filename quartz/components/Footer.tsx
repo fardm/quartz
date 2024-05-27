fc47a323b92dfe9256177bdf3a33882a9d5551ed
fardm <146080698+fardm@users.noreply.github.com>
2024-05-27 17:47:18 +0330

    Quartz sync: May 27, 2024, 5:47 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,9 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <hr />
-        <p>          
-        
-        </p>
+
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
@@ -24,6 +22,18 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
+        
+        <br/>
+
+        <p>          
+          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          <a href="https://quartz.jzhao.xyz/">Quartz</a>
+        </p>
+
+        <p>
+          {year}©        
+        </p>
+
       </footer>
     )
   }
