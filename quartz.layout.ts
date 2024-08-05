95bef30ee76bf15152e0bc78af5471f80aca701c
fardm <146080698+fardm@users.noreply.github.com>
2024-08-05 18:14:46 +0330

    Quartz sync: Aug 5, 2024, 6:14 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'fardm/quartz',
+        // from data-repo-id
+        repoId: 'R_kgDOL-drYg',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOL-drYs4ChaaZ',
+        lang: 'fa',
+        loading: 'lazy',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       تلگرام: "https://t.me/ifard_ir/",
